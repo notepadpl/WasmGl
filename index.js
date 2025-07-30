a9ca3e14773b36c6036df63ad0a11bdd7731fbef
notepadpl <notepadpl@users.noreply.github.com>
2025-07-30 06:02:54 +0000

    deploy: a3fa1e0bd10b5c6c34776f746c87cc1c859680bc

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpyk5zxsqp.js
+// include: /tmp/tmp80kfhy_r.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -220,21 +220,21 @@ Module['FS_createPath']("/", "asserts", true, true);
 
   })();
 
-// end include: /tmp/tmpyk5zxsqp.js
-// include: /tmp/tmpnizeu5v2.js
+// end include: /tmp/tmp80kfhy_r.js
+// include: /tmp/tmpds99tb1g.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpnizeu5v2.js
-// include: /tmp/tmp_izwy_yo.js
+  // end include: /tmp/tmpds99tb1g.js
+// include: /tmp/tmpu2m79n0m.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp_izwy_yo.js
+  // end include: /tmp/tmpu2m79n0m.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -9188,6 +9188,7 @@ var ASM_CONSTS = {
 
 
 
+
   var listenOnce = (object, event, func) => {
       object.addEventListener(event, func, { 'once': true });
     };
@@ -9780,7 +9781,9 @@ var wasmImports = {
   /** @export */
   glUseProgram: _glUseProgram,
   /** @export */
-  glVertexAttribPointer: _glVertexAttribPointer
+  glVertexAttribPointer: _glVertexAttribPointer,
+  /** @export */
+  glViewport: _glViewport
 };
 var wasmExports = createWasm();
 var ___wasm_call_ctors = createExportWrapper('__wasm_call_ctors', 0);
