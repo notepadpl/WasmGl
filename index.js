fc30999fb04a9b8604cc717dcf3b7741453f0451
notepadpl <notepadpl@users.noreply.github.com>
2025-07-30 20:56:11 +0000

    deploy: 551a2015f719add1458a57e27da51d372f3d50f8

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpfso56_uz.js
+// include: /tmp/tmput7usbp4.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -216,25 +216,25 @@ Module['FS_createPath']("/", "asserts", true, true);
     }
 
     }
-    loadPackage({"files": [{"filename": "/asserts/cubbe.obj", "start": 0, "end": 1560}, {"filename": "/asserts/cube.mtl", "start": 1560, "end": 1663}, {"filename": "/asserts/cube.obj", "start": 1663, "end": 2149}, {"filename": "/asserts/cube2.obj", "start": 2149, "end": 2522}, {"filename": "/asserts/cube_texture.png", "start": 2522, "end": 36850}, {"filename": "/asserts/tmp", "start": 36850, "end": 36854}], "remote_package_size": 36854});
+    loadPackage({"files": [{"filename": "/asserts/cubbe.obj", "start": 0, "end": 1560}, {"filename": "/asserts/cube.mtl", "start": 1560, "end": 1663}, {"filename": "/asserts/cube.obj", "start": 1663, "end": 2149}, {"filename": "/asserts/cube2.obj", "start": 2149, "end": 2522}, {"filename": "/asserts/cube_texture.jpg", "start": 2522, "end": 29501}, {"filename": "/asserts/cube_texture.png", "start": 29501, "end": 63829}, {"filename": "/asserts/tmp", "start": 63829, "end": 63833}], "remote_package_size": 63833});
 
   })();
 
-// end include: /tmp/tmpfso56_uz.js
-// include: /tmp/tmpbdn8sxg2.js
+// end include: /tmp/tmput7usbp4.js
+// include: /tmp/tmp2j6rnjrq.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpbdn8sxg2.js
-// include: /tmp/tmp2rskgw9m.js
+  // end include: /tmp/tmp2j6rnjrq.js
+// include: /tmp/tmp7v0kdmiw.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp2rskgw9m.js
+  // end include: /tmp/tmp7v0kdmiw.js
 
 
 // Sometimes an existing Module object exists with properties
